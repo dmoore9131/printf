b6a81f7d6de153535c0a4df3c0ee450e86ea92db
dmoore9131 <dominiquemoore776@gmail.com>
2023-07-23 10:42:13 -0700

    task0-8

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /* Print a message to the console. */
+printf("Hello, World!\n");
+
+/* Return 0 to indicate successful program execution. *;
+return 0;
+}
+
