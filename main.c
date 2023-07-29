19bdb6d3659cba4b707c47e4c73b0deb9b614dee
dmoore9131 <dominiquemoore776@gmail.com>
2023-07-28 19:31:27 -0700

    task1

--- main.c
+++ main.c
@@ -1,12 +1,37 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
-{
-    int len;
+// Function to convert the integer value and write to the output file
+void convertAndWrite(FILE *output, int value) {
+    fprintf(output, "%d\n", value);
+}
 
-    len = _printf("Hello, %c! This is a %s test %%\n", 'W', "printf");
-    _printf("Number of characters printed: %d\n", len);
+int main() {
+    FILE *input, *output;
+    input = fopen("input.txt", "r");
+    output = fopen("output.txt", "w");
 
-    return (0);
+    if (input == NULL || output == NULL) {
+        printf("Error opening files.\n");
+        return 1;
+    }
+
+    char format[5];
+    int value;
+
+    // Read input from the input file and process until the end
+    while (fscanf(input, "%4s %d", format, &value) != EOF) {
+        if (format[0] == '%' && (format[1] == 'd' || format[1] == 'i')) {
+            convertAndWrite(output, value);
+        } else {
+            fprintf(output, "Invalid format: %s\n", format);
+        }
+    }
+
+    fclose(input);
+    fclose(output);
+
+    printf("Conversion completed. Check the output.txt file.\n");
+    return 0;
 }
 
